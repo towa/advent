20b1305c286108d9101d4d1d811b67c0b8b66b17
towa <7078986+towa@users.noreply.github.com>
2023-12-08 08:05:11 +0100

    lots of changes

--- advent/__main__.py
+++ advent/__main__.py
@@ -1,20 +1,46 @@
-import sys
 import importlib
+import click
+from rich import print
 
 from advent.api import AdventAPI
 
 
-def main():
-    a = AdventAPI()
+@click.group()
+@click.option("--token")
+@click.option("--day", "-d")
+@click.option("--year", "-y")
+@click.pass_context
+def cli(ctx, token, day, year):
+    ctx.obj = AdventAPI(token=token, year=year, day=day)
+
+
+@cli.command()
+@click.pass_obj
+def desc(obj):
+    obj.show_description()
+
+
+@cli.command()
+@click.argument("module", required=True)
+@click.argument("function")
+@click.option("--test", "-t", is_flag=True)
+@click.pass_obj
+def run(obj, module, function, test):
     try:
-        day_mod = importlib.import_module(f"advent.days.{sys.argv[1]}")
+        mod = importlib.import_module(module)
     except ModuleNotFoundError as e:
         print(e.msg)
         return
 
-    solver = day_mod.DaySolver(a)
-    solver.run_solve()
+    if function and function in dir(mod):
+        if test:
+            input = obj.get_test_input()
+        else:
+            input = obj.get_input()
+        res = getattr(mod, function)(input)
+        print(
+            f"The function [italic red]{function}[/italic red] returns {res}")
 
 
 if __name__ == "__main__":
-    main()
+    cli()
--- advent/api.py
+++ advent/api.py
@@ -1,26 +1,65 @@
 # Automatically get inputs from AOC and check solutions
-import requests
 import os
+from datetime import date
+
+import requests
+from markdownify import markdownify
+from rich.console import Console
+from rich.markdown import Markdown
+from rich import print
+from lxml import etree
 
 
 class AdventAPI():
-    def __init__(self, token=None, year=2023):
+    def __init__(self, token=None, day=None, year=None):
         if not token:
             token = os.environ.get('AOC_TOKEN')
+        if not year:
+            year = date.today().year
+        if not day:
+            day = (date.today() - date.fromisoformat(f"{year}-11-30")).days
         self.session_cookie = {'session': token}
-        self.base_url = f"https://adventofcode.com/{year}"
+        self.base_url = f"https://adventofcode.com/{year}/day/{day}"
+        self.day = day
         self.year = year
 
-    def get_input(self, day):
+    def get_input(self):
         res = requests.get(
-            f"{self.base_url}/day/{day}/input",
+            f"{self.base_url}/input",
             cookies=self.session_cookie
         )
         return res.content.decode().splitlines()
+    
+    def get_test_input(self):
+        res = requests.get(
+            self.base_url,
+            cookies=self.session_cookie
+        )
+        root = etree.HTML(res.content.decode())
+        code = root.xpath("//pre/code")
+        if code:
+            return code[0].text.splitlines()
+        else:
+            return None
 
-    def post_answer(self, day, level, answer):
+    def post_answer(self, level, answer):
         payload = {'level': level, 'answer': answer}
         res = requests.post(
-            f"{self.base_url}/day/{day}/answer",
+            f"{self.base_url}/answer",
             data=payload, cookies=self.session_cookie)
         print(res.content)
+
+    def show_description(self):
+        res = requests.get(
+            self.base_url,
+            cookies=self.session_cookie
+        )
+        console = Console()
+        html_str = res.content.decode()
+        root = etree.HTML(html_str)
+        article = root.xpath('//article')
+        if article:
+            md = Markdown(markdownify(etree.tostring(article[0])))
+            console.print(md)
+        else:
+            print(f"[bold]No entry for year {self.year} day {self.day}")
